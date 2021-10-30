926838451820a437bbd4ad3c7812e55458ee44e9
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-30 04:49:45 +0100

    adding custom update input

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { nanoid } from "nanoid";
 
 const TodoForm = (props) => {
-    const [input, setInput] = useState("");
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
     const focusRef = useRef(null)
 
     useEffect(() => {
@@ -26,16 +26,35 @@ const TodoForm = (props) => {
 
     return (
         <form className="todo-form" onSubmit={handleSumbit}>
-            <input
-                type="text"
-                placeholder="Add a task"
-                value={input}
-                name="text"
-                className="todo-input"
-                onChange={handleInputChange}
-                ref={focusRef}
-            />
-            <button className="todo-button">Add todo</button>
+            {props.edit ? (
+                <>
+                    <input
+                        placeholder="Update your item"
+                        value={input}
+                        onChange={handleInputChange}
+                        name="text"
+                        ref={focusRef}
+                        className="todo-input edit"
+                    />
+                    <button onClick={handleSumbit} className="todo-button edit">
+                        Update
+                    </button>
+                </>
+            ) : (
+                <>
+                    <input
+                        placeholder="Add a todo"
+                        value={input}
+                        onChange={handleInputChange}
+                        name="text"
+                        className="todo-input"
+                        ref={focusRef}
+                    />
+                    <button onClick={handleSumbit} className="todo-button">
+                        Add todo
+                    </button>
+                </>
+            )}
         </form>
     );
 };
